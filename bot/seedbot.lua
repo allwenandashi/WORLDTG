ac24497f1422f39555fcd1c2f683bb8e511cc5ec
imashiam <ashkanpj.ap2@gmail.com>
2016-06-22 22:48:20 +0300

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,9 +230,10 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks"
+	"msg_checks",
+	"plugins"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {198475619},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[WORLD BOT v9
 An advanced administration bot based on TG-CLI written in Lua
